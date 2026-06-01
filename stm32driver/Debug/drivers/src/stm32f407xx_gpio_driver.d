5b123ad25a9709ac62b39fb29fbf50239ea24a97
Kiran Badiger <157112116+kiranbad@users.noreply.github.com>
2026-06-01 07:50:51 +0530

    Delete stm32driver directory

--- stm32driver/Debug/drivers/src/stm32f407xx_gpio_driver.d
+++
@@ -1,8 +0,0 @@
-drivers/src/stm32f407xx_gpio_driver.o: \
- ../drivers/src/stm32f407xx_gpio_driver.c \
- D:/005_STM32/stm32driver/drivers/inc/stm32f407xx_gpio_driver.h \
- D:/005_STM32/stm32driver/drivers/inc/stm32f407xx.h \
- D:/005_STM32/stm32driver/drivers/inc/stm32f407xx.h
-D:/005_STM32/stm32driver/drivers/inc/stm32f407xx_gpio_driver.h:
-D:/005_STM32/stm32driver/drivers/inc/stm32f407xx.h:
-D:/005_STM32/stm32driver/drivers/inc/stm32f407xx.h:
